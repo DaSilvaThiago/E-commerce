5e71ce069b78bafcc3589135b70b19d914dff96a
Thiago Dias da Silva <silvathiago011@hotmail.com>
2023-10-20 00:03:24 -0300

    organizing

--- website/js/main.js
+++ website/js/main.js
@@ -21,6 +21,7 @@ for (let i = 0; i < triggerOpen.length; i++) {
 }
 
 //menu mobile
+
 const submenu = document.querySelectorAll('.child-trigger');
 submenu.forEach((menu) => menu.addEventListener('click', function(e) {
     e.preventDefault();
@@ -29,3 +30,20 @@ submenu.forEach((menu) => menu.addEventListener('click', function(e) {
         this.closest('.has-child').classList.toggle('active');
     }
 }))
+
+
+//slider
+
+const swiper = new Swiper('.sliderbox', {
+
+    loop: true,
+    effect: 'fade',
+    autoHeoght: true,
+    
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+  });
