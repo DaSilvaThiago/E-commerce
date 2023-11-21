981825d12a73813e02b2d37afe85d2596e91f2d4
Thiago Dias da Silva <silvathiago011@hotmail.com>
2023-11-21 17:55:25 -0300

    going to build order page

--- pi/resources/views/profile/cart.blade.php
+++ pi/resources/views/profile/cart.blade.php
@@ -60,11 +60,15 @@
             <div class="section__content">
                 <div class="container">
                     <div class="row">
-                        <div class="col-lg-12 col-md-12 col-sm-12 u-s-m-b-30">
+                        <div class="col-lg-8 col-md-6 u-s-m-b-30">
                             <div class="table-responsive">
                                 <table class="table-p">
                                     <tbody>
-
+                                        @php
+                                            $subtotal = 0;
+                                            $total = 0;
+                                            $descontos = 0;
+                                        @endphp
                                         <!--====== Row ======-->
                                         @foreach ($productsByUser as $item)
                                             @if (!$item->ITEM_QTD == 0)
@@ -105,16 +109,12 @@
                                                             <!--====== Input Counter ======-->
                                                             <div class="input-counter">
 
-                                                                <span
-                                                                    class="input-counter__minus fas fa-minus buttonToAddOrGetOut"></span>
-
                                                                 <input
                                                                     class="input-counter__text input-counter--text-primary-style"
                                                                     type="text" value="{{ $item->ITEM_QTD }}"
-                                                                    data-min="1" data-max="1000">
+                                                                    data-min="1" readonly data-max="1000">
 
-                                                                <span
-                                                                    class="input-counter__plus fas fa-plus buttonToAddOrGetOut"></span>
+
                                                             </div>
                                                             <!--====== End - Input Counter ======-->
                                                         </div>
@@ -129,6 +129,11 @@
                                                         </div>
                                                     </td>
                                                 </tr>
+                                                @php
+                                                    $subtotal += $item->ITEM_QTD * $item->produto->PRODUTO_PRECO;
+                                                    $total += $item->ITEM_QTD * ($item->produto->PRODUTO_PRECO - $item->produto->PRODUTO_DESCONTO);
+                                                    $descontos += $item->produto->PRODUTO_DESCONTO;
+                                               @endphp
                                             @endif
                                         @endforeach
                                         <!--====== End - Row ======-->
@@ -137,11 +142,40 @@
                                 </table>
                             </div>
                         </div>
+                        <div class="col-lg-4 col-md-6 u-s-m-b-30">
+                            <form class="f-cart" action="">
+                                <div class="f-cart__pad-box">
+                                    <div class="u-s-m-b-30">
+                                        <table class="f-cart__table">
+                                            <tbody>
+                                                <tr>
+                                                    <td>SUBTOTAL</td>
+                                                    <td>R${{ $subtotal }}</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>DESCONTOS</td>
+                                                    <td>R${{ $descontos }}</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>TOTAL</td>
+                                                    <td>R${{ $total }}</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <div>
+
+                                        <button class="btn btn--e-brand-b-2" type="submit"> FINALIZAR COMPRA</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
                         <div class="col-lg-12">
                             <div class="route-box">
                                 <div class="route-box__g1">
 
-                                    <a class="route-box__link" href="search?CATEGORIA_ID={{ $item->produto->categoria->CATEGORIA_ID }}"><i
+                                    <a class="route-box__link"
+                                        href="search?CATEGORIA_ID={{ $item->produto->categoria->CATEGORIA_ID }}"><i
                                             class="fas fa-long-arrow-alt-left"></i>
 
                                         <span>CONTINUAR COMPRANDO</span></a>
@@ -150,135 +184,18 @@
 
                                     <a class="route-box__link" href="cart.html"><i class="fas fa-trash"></i>
 
-                                        <span>CLEAR CART</span></a>
+                                        <span>LIMPAR CARRINHO</span></a>
 
-                                    <a class="route-box__link" href="cart.html"><i class="fas fa-sync"></i>
-
-                                        <span>UPDATE CART</span></a>
                                 </div>
                             </div>
                         </div>
+
                     </div>
                 </div>
             </div>
             <!--====== End - Section Content ======-->
         </div>
         <!--====== End - Section 2 ======-->
-
-
-        <!--====== Section 3 ======-->
-        <div class="u-s-p-b-60">
-
-            <!--====== Section Content ======-->
-            <div class="section__content">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-12 col-md-12 col-sm-12 u-s-m-b-30">
-                            <form class="f-cart">
-                                <div class="row">
-                                    <div class="col-lg-4 col-md-6 u-s-m-b-30">
-                                        <div class="f-cart__pad-box">
-                                            <h1 class="gl-h1">ESTIMATE SHIPPING AND TAXES</h1>
-
-                                            <span class="gl-text u-s-m-b-30">Enter your destination to get a shipping
-                                                estimate.</span>
-                                            <div class="u-s-m-b-30">
-
-                                                <!--====== Select Box ======-->
-
-                                                <label class="gl-label" for="shipping-country">COUNTRY *</label><select
-                                                    class="select-box select-box--primary-style" id="shipping-country">
-                                                    <option selected value="">Choose Country</option>
-                                                    <option value="uae">United Arab Emirate (UAE)</option>
-                                                    <option value="uk">United Kingdom (UK)</option>
-                                                    <option value="us">United States (US)</option>
-                                                </select>
-                                                <!--====== End - Select Box ======-->
-                                            </div>
-                                            <div class="u-s-m-b-30">
-
-                                                <!--====== Select Box ======-->
-
-                                                <label class="gl-label" for="shipping-state">STATE/PROVINCE
-                                                    *</label><select class="select-box select-box--primary-style"
-                                                    id="shipping-state">
-                                                    <option selected value="">Choose State/Province</option>
-                                                    <option value="al">Alabama</option>
-                                                    <option value="al">Alaska</option>
-                                                    <option value="ny">New York</option>
-                                                </select>
-                                                <!--====== End - Select Box ======-->
-                                            </div>
-                                            <div class="u-s-m-b-30">
-
-                                                <label class="gl-label" for="shipping-zip">ZIP/POSTAL CODE *</label>
-
-                                                <input class="input-text input-text--primary-style" type="text"
-                                                    id="shipping-zip" placeholder="Zip/Postal Code">
-                                            </div>
-                                            <div class="u-s-m-b-30">
-
-                                                <a class="f-cart__ship-link btn--e-transparent-brand-b-2"
-                                                    href="cart.html">CALCULATE SHIPPING</a>
-                                            </div>
-
-                                            <span class="gl-text">Note: There are some countries where free shipping is
-                                                available otherwise our flat rate charges or country delivery charges
-                                                will be apply.</span>
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-4 col-md-6 u-s-m-b-30">
-                                        <div class="f-cart__pad-box">
-                                            <h1 class="gl-h1">NOTE</h1>
-
-                                            <span class="gl-text u-s-m-b-30">Add Special Note About Your Product</span>
-                                            <div>
-
-                                                <label for="f-cart-note"></label>
-                                                <textarea class="text-area text-area--primary-style" id="f-cart-note"></textarea>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-4 col-md-6 u-s-m-b-30">
-                                        <div class="f-cart__pad-box">
-                                            <div class="u-s-m-b-30">
-                                                <table class="f-cart__table">
-                                                    <tbody>
-                                                        <tr>
-                                                            <td>SHIPPING</td>
-                                                            <td>$4.00</td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td>TAX</td>
-                                                            <td>$0.00</td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td>SUBTOTAL</td>
-                                                            <td>$379.00</td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td>GRAND TOTAL</td>
-                                                            <td>$379.00</td>
-                                                        </tr>
-                                                    </tbody>
-                                                </table>
-                                            </div>
-                                            <div>
-
-                                                <button class="btn btn--e-brand-b-2" type="submit"> PROCEED TO
-                                                    CHECKOUT</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!--====== End - Section Content ======-->
-        </div>
-        <!--====== End - Section 3 ======-->
     </div>
     <!--====== End - App Content ======-->
 
